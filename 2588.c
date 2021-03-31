1971a4b1975a62f13202ccbc9c3c7581a25dd79d
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-04-01 00:17:13 +0900

    Update 2588.c

--- 2588.c
+++ 2588.c
@@ -6,7 +6,7 @@ int main(void)
     scanf("%d %d",&A,&B);
     
     int out1 = A*((B%100)%10);
-    int out2 = A*(B%100)/10;
+    int out2 = A*((B%100)/10);
     int out3 = A*(B/100);
     mul = A*B;
     printf("%d\n%d\n%d\n%d",out1,out2,out3,mul);
