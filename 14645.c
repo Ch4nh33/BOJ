e2089bfa9205bfe41bb8d14e01ae21af11452c6b
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-04-01 17:24:37 +0900

    Create 14645.c

---
+++ 14645.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int N;
+  int K;
+  for (size_t i = 0; ; i++)
+  {
+    scanf("%d %d",&N,&K);
+    if(N ==0)
+    {break;}
+  }
+  printf("비와이");
+
+   return 0;
+}
